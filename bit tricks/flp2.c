3ae3aae764b24ca7ef24e3bccb0a1ecf3be217e4
armandli <armand.li@hotmail.com>
2013-04-05 00:05:50 -0400

    add some bit tricks
    find the greatest power of 2 <= x and the least power of 2 >= x

---
+++ bit tricks/flp2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+//finds the greatest power of 2 less and equal to x, found in Hacker's Delight
+//14 instructions, no loop
+unsigned flp2(unsigned x){
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  return x - (x >> 1);
+}
+
+//find the least power of 2 greater or equal to x
+unsigned clp2(unsigned x){
+  x = x - 1;
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  return x + 1;
+}
+
+int main(){
+  unsigned eight = flp2(8);
+  printf("8 = %u\n", eight);
+
+  unsigned seven = flp2(7);
+  printf("7 = %u\n", seven);
+}
