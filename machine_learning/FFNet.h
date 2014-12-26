aa569c2b43bb70de7b069580773a9ab760783f85
armandli <armand.li@hotmail.com>
2014-12-25 23:46:17 -0500

    added cost function trait; regularization function trait; redefined epoch to be total number of iterations on entire dataset, instead of number of iterations on mini-batches regardless of total data size; implemented save and load function; added linear activation function

--- machine_learning/FFNet.h
+++ machine_learning/FFNet.h
@@ -1,12 +1,20 @@
+#include <cassert>
 #include <cstddef>
 #include <cmath>
-#include <cassert>
+#include <limits>
 #include <vector>
+#include <string>
 #include <random>
 #include <armadillo>
 
 //TODO: there is an issue with STL random generator unable to produce the upper
 //bound value; need to double check
+//TODO: add optional output layer only activation function
+//TODO: add optional softmax option
+
+#ifndef NN_FORMAT
+#define NN_FORMAT "arma_binary"
+#endif
 
 /* Feed Forward Neural Network */
 
@@ -33,6 +41,30 @@ namespace NN {
       return f.deriviative(m);
       }
     };
+
+  /* Cost Function Trait */
+  template <template <template <typename> class, typename> class CostFun,
+            template <typename> class ActFun,
+            typename T = double>
+  struct CostFunTrait {
+    static arma::Mat<T> cost(const arma::Mat<T>& z, const arma::Mat<T>& e){
+      CostFun<ActFun, T> f;
+      return f.cost(z, e);
+      }
+    static arma::Mat<T> deriviative(const arma::Mat<T>& z, const arma::Mat<T>& e){
+      CostFun<ActFun, T> f;
+      return f.deriviative(z, e);
+      }
+    };
+
+  /* Regularization Function Trait for weight matrix */
+  template <template <typename> class RegFun, typename T = double>
+  struct RegFunTrait {
+    static void regularize(arma::Mat<T>& w, const arma::Mat<T>& dw, T learn, T lambda){
+      RegFun<T> f;
+      f.regularize(w, dw, learn, lambda);
+      }
+    };
   
   /* Matrix Initialization Function Trait */
   template <template <typename> class InitFun, typename T = double>
@@ -41,6 +73,13 @@ namespace NN {
       InitFun<T> f;
       f.initialize(m, isFirstLayer);
       }
+    };
+
+  //TODO: maybe we should combine normalization function and activation function together
+  /* Linear Normalization Function */
+  template <typename T = double>
+  struct LinearNormFun {
+    void normalize(arma::Mat<T>&){ /*do nothing*/ }
     };
 
   /* Sigmoid Normalization Function */
@@ -65,6 +104,20 @@ namespace NN {
       }
     };
   
+  /* Linear Activation Function */
+  template <typename T = double>
+  struct LinearFun {
+    arma::Mat<T> function(const arma::Mat<T>& m){
+      arma::Mat<T> r = m;
+      return r;
+      }
+    arma::Mat<T> deriviative(const arma::Mat<T>& m){
+      arma::Mat<T> r(m.n_rows, m.n_cols);
+      r.fill(1.);
+      return r;
+      }
+    };
+
   /* Sigmoid Activation Function */
   template <typename T = double>
   struct SigmoidFun {
@@ -85,8 +138,7 @@ namespace NN {
 
   /* Hyperbolic Tangent Activation Function */
   template <typename T = double>
-  class HTanFun {
-  public:
+  struct HTanFun {
     arma::Mat<T> function(const arma::Mat<T>& m){
       arma::Mat<T> r = m;
       r.transform( [](T v){ return std::tanh(v); } );
@@ -98,8 +150,69 @@ namespace NN {
       return r;
       }
     };
+
+  /* Quadradic Cost Function */
+  template <template <typename> class ActFun, typename T = double>
+  struct QCost {
+    //TODO
+    arma::Mat<T> cost(const arma::Mat<T>& z, const arma::Mat<T>& e){
+      T n = arma::norm(z - e, 2);
+      return 0.5 * n * n;
+      }
+    arma::Mat<T> deriviative(const arma::Mat<T>& z, const arma::Mat<T>& e){
+      return (ActFunTrait<ActFun,T>::function(z) - e) % ActFunTrait<ActFun,T>::deriviative(z);
+      }
+    };
+
+  /* Cross Entropy Cost Function */
+  template <template <typename> class ActFun, typename T = double>
+  struct CECost {
+    //TODO
+    arma::Mat<T> cost(const arma::Mat<T>& z, const arma::Mat<T>& e){
+      arma::Mat<T> a = ActFunTrait<ActFun,T>::function(z);
+      return arma::sum(arma::sum(-1 % e % arma::log(a) - (1 - e) % arma::log(1 - a))) / (T)z.n_rows;
+      }
+    arma::Mat<T> deriviative(const arma::Mat<T>& z, const arma::Mat<T>& e){
+      return ActFunTrait<ActFun,T>::function(z) - e;
+      }
+    };
+
+  /* No-Regularization Regularization Function */
+  template <typename T = double>
+  struct NRRegFun {
+    void regularize(arma::Mat<T>& w, const arma::Mat<T>& dw, T, T){
+      w -= dw;
+      }
+    };
+
+  /* L2 Regularization Function */
+  template <typename T = double>
+  struct L2RegFun {
+    void regularize(arma::Mat<T>& w, const arma::Mat<T>& dw, T learn, T lambda){
+      w = (1. - learn * lambda) * w - dw;
+      }
+    };
+
+  /* L1 Regularization Function */
+  template <typename T = double>
+  struct L1RegFun {
+    void regularize(arma::Mat<T>& w, const arma::Mat<T>& dw, T learn, T lambda){
+      w -= (learn * lambda) * signFunc(w) + dw;
+      }
+  private:
+    arma::Mat<T> signFunc(const arma::Mat<T>& w){
+      arma::Mat<T> sw = w;
+      w.transform( [](T v){ return v < std::numeric_limits<T>::epsilon() &&
+                                  -1. * v < std::numeric_limits<T>::epsilon() ? 0. :
+                                  (v > 0.                                     ? 1. :
+                                                                               -1.);
+                          }
+                 );
+      return sw;
+      }
+    };
   
-  /* Random Postive Initialization Function; random value between 0. to 1. */
+  /* Random Postive Initialization Function; value [0.,1.] with uniform distribution */
   template <typename T = double>
   struct RndPInit {
     void initialize(arma::Mat<T>& m, bool){
@@ -111,7 +224,7 @@ namespace NN {
       }
     };
   
-  /* Random Initialization Function; random value between -1. to 1. */
+  /* Random Initialization Function; value [-1.,1.] with uniform distribution */
   template <typename T = double>
   struct RndNInit {
     void initialize(arma::Mat<T>& m, bool){
@@ -119,6 +232,18 @@ namespace NN {
       std::default_random_engine generator(seed);
       std::uniform_real_distribution<T> distribution(-1., 1.);
   
+      m.imbue( [&distribution, &generator](){ return distribution(generator); } );
+      }
+    };
+
+  /* Random Initialization Function; value [-1.,1.] with Gaussian Distribution of mean 0 */
+  template <typename T = double>
+  struct GauNInit {
+    void initialization(arma::Mat<T>& m, bool){
+      unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+      std::default_random_engine generator(seed);
+      std::normal_distribution<T> distribution(-1., 1.);
+
       m.imbue( [&distribution, &generator](){ return distribution(generator); } );
       }
     };
@@ -149,7 +274,10 @@ namespace NN {
   template <size_t ... Hs> class HiddenLayer{};
 
   /* Feed Forward Neural Network of fixed number of hidden layers */
-  template <class Derived, template <typename> class ActFunc, template <typename> class InitFunc, typename T>
+  template <class Derived,
+            template <typename> class ActFunc,
+            template <typename> class InitFunc,
+            typename T>
   class FFNet {
   protected:
     /* weight matrix between each neuron layer,
@@ -162,11 +290,12 @@ namespace NN {
      * Col: number of neuron in the layer
      */
     std::vector<arma::Mat<T>> mBMtx;
-    /* output of neurons in each layer, but not include input layer.
+    /* non-activated output of neurons in each layer, but not include input layer.
      * Row: # of input
      * Col: # of neuron in this layer
      */
     std::vector<arma::Mat<T>> mLMtx;
+    //TODO: save the activation output as well to avoid computation
     size_t mInSize;
     size_t mOutSize;
     size_t mEpoch;
@@ -176,11 +305,11 @@ namespace NN {
       mLMtx.clear();
   
       arma::Mat<T> d = im * mWMtx[0] + arma::repmat(mBMtx[0], im.n_rows, 1);
-      arma::Mat<T> e = ActFunTrait<ActFunc>::function(d);
+      arma::Mat<T> e = ActFunTrait<ActFunc,T>::function(d);
       mLMtx.push_back(d);
       for (size_t i = 1; i < mWMtx.size(); ++i){
         d = e * mWMtx[i] + arma::repmat(mBMtx[i], im.n_rows, 1);
-        e = ActFunTrait<ActFunc>::function(d);
+        e = ActFunTrait<ActFunc,T>::function(d);
         mLMtx.push_back(d);
         }
       }
@@ -217,12 +346,32 @@ namespace NN {
 
     /* save the neural network to file*/
     void save(const std::string& filename){
-      //TODO
+      size_t cnt = 0;
+      for (auto& mtx : mWMtx){
+        std::string name = filename;
+        name += std::to_string(cnt++);
+        mtx.save(name.c_str(), NN_FORMAT);
+        }
+      for (auto& mtx : mBMtx){
+        std::string name = filename;
+        name += std::to_string(cnt++);
+        mtx.save(name.c_str(), NN_FORMAT);
+        }
       }
 
     /* load neural network from file */
     void load(const std::string& filename){
-      //TODO
+      size_t cnt = 0;
+      for (auto& mtx : mWMtx){
+        std::string name = filename;
+        name += std::to_string(cnt++);
+        mtx.load(name.c_str(), NN_FORMAT);
+        }
+      for (auto& mtx : mBMtx){
+        std::string name = filename;
+        name += std::to_string(cnt++);
+        mtx.load(name.c_str(), NN_FORMAT);
+        }
       }
   
     /* Train the neural network; return training result matrix for error calculation*/
@@ -236,7 +385,7 @@ namespace NN {
       arma::Mat<T> out = data.cols(mInSize, data.n_cols - 1);
 
       feedForward(in);
-      return ActFunTrait<ActFunc>::function(mLMtx.back()) - out;
+      return out - ActFunTrait<ActFunc,T>::function(mLMtx.back());
       }
   
     /* Test the neural network; return test result matrix for error calculation*/
@@ -247,28 +396,33 @@ namespace NN {
       arma::Mat<T> out = data.cols(mInSize, data.n_cols - 1);
 
       feedForward(in);
-      return ActFunTrait<ActFunc>::function(mLMtx.back()) - out;
+      return out - ActFunTrait<ActFunc,T>::function(mLMtx.back());
       }
   
     arma::Mat<T> predict(const arma::Mat<T>& in){
       feedForward(in);
-      return ActFunTrait<ActFunc>::function(mLMtx.back());
+      return ActFunTrait<ActFunc,T>::function(mLMtx.back());
       }
     };
   
   /* Feed Forward Neural Network; paired with Back Propagation using stochastic gradient descent */
-  template <template <typename> class ActFunc, template <typename> class InitFunc, typename T = double>
-  class BPFFNet : public FFNet<BPFFNet<ActFunc, InitFunc, T>, ActFunc, InitFunc, T> {
+  template <template <template <typename> class, typename> class CostFunc,
+            template <typename> class ActFunc,
+            template <typename> class InitFunc,
+            template <typename> class RegFunc = NRRegFun,
+            typename T = double>
+  class BPFFNet : public FFNet<BPFFNet<CostFunc, ActFunc, InitFunc, RegFunc, T>, ActFunc, InitFunc, T> {
     /* previous change to weight matrix for momentum calculation */
     std::vector<arma::Mat<T>> mDWMtx;
-    double mLearn;
-    double mMomentum;
+    T mLearn;
+    T mRegu;
+    T mMomentum;
     size_t mBatchSize;
   public:
     template <size_t ... Hs>
-    BPFFNet(HiddenLayer<Hs...> hidden, size_t in, size_t out, double learning_rate, double momentum, size_t batch_size, size_t epoch) :
-      FFNet<BPFFNet<ActFunc, InitFunc, T>, ActFunc, InitFunc, T>::FFNet(hidden, in, out, epoch),
-      mLearn(learning_rate), mMomentum(momentum), mBatchSize(batch_size) {
+    BPFFNet(HiddenLayer<Hs...> hidden, size_t in, size_t out, T learning_rate, T lambda, T momentum, size_t batch_size, size_t epoch) :
+      FFNet<BPFFNet<CostFunc, ActFunc, InitFunc, RegFunc, T>, ActFunc, InitFunc, T>::FFNet(hidden, in, out, epoch),
+      mLearn(learning_rate), mRegu(lambda), mMomentum(momentum), mBatchSize(batch_size) {
       size_t HiddenLayersSizes[] = { Hs ... };
 
       size_t mRows = in;
@@ -290,96 +444,55 @@ namespace NN {
       size_t mInSize = this->mInSize;
       size_t mOutSize = this->mOutSize;
       size_t mEpoch = this->mEpoch;
-
-      arma::Mat<T> sdata = arma::shuffle(data, 0);
+      T regLearn = mLearn * mBatchSize / data.n_rows;
 
       for (size_t i = 0; i < mEpoch; ++i){
-        mLMtx.clear();
-        size_t start = i * sdata.n_rows / mEpoch % sdata.n_rows;
-        size_t end = start + mBatchSize >= sdata.n_rows ? sdata.n_rows - 1 : start + mBatchSize - 1;
-        arma::Mat<T> in = sdata.submat(start, 0, end, mInSize - 1);
-        arma::Mat<T> out = sdata.submat(start, mInSize, end, sdata.n_cols - 1);
+        arma::Mat<T> sdata = arma::shuffle(data, 0);
+        for (size_t start = 0, end = mBatchSize >= sdata.n_rows ? sdata.n_rows : mBatchSize;
+             start < sdata.n_rows;
+             start = end, end = start + mBatchSize >= sdata.n_rows ? sdata.n_rows : start + mBatchSize){
+          arma::Mat<T> in = sdata.submat(start, 0, end - 1, mInSize - 1);
+          arma::Mat<T> out = sdata.submat(start, mInSize, end - 1, sdata.n_cols - 1);
 
-        // feed forward step for back propagation
-        this->feedForward(in);
-
-        // back propagation
-        /* Output layer Gradient Descent
-         *   Del[N*O] = (E[N*O] - R[N*O]) % f'(L[N*O])
-         *   G[I*O]   = P[N*I]^t * Del[N*O] 
-         *   DW@t+1   = l * G + m * DW@t
-         *   DB@t+1   = Del
-         *     where
-         *     N   = number of input case
-         *     O   = output layer neuron size
-         *     I   = previous layer neuron size, this could be input player if no hidden layer is present
-         *     Del = delta matrix
-         *     L   = output layer before applying activation function
-         *     G   = gradient matrix
-         *     P   = previous layer output
-         *     DW  = change to weight matrix before output layer before the previous layer
-         *     l   = learning rate
-         *     m   = momentum
-         *
-         * Inner Layer (non-output layer) Gradient Descent
-         *   Del[N*O] = Del1[N*O1] * W1[O*O1]^t % f'(L[N*O])
-         *   G[I*O]   = P[N*I]^t * Del[N*O]
-         *   DW@t+1   = l * G + m * DW@t
-         *   DB@t+1   = Del
-         *     where
-         *     N    = number of input case
-         *     O1   = number of output in next layer
-         *     O    = number of output in this layer, or input in next layer
-         *     I    = number of input in this layer
-         *     Del  = delta matrix
-         *     Del1 = delta matrix of next layer
-         *     W1   = weight matrix of next layer (before update)
-         *     L    = output before applying activation function for this layer
-         *     G    = gradient matrix
-         *     P    = input from previous layer
-         *     DW   = change to weight matrix before this layer and previous layer
-         *     l    = learning rate
-         *     m    = momentum
-         */
-        arma::Mat<T> d = (out - ActFunTrait<ActFunc>::function(mLMtx.back())) % ActFunTrait<ActFunc>::deriviative(mLMtx.back());
-        const arma::Mat<T>& p = mLMtx.size() == 1 ? in : ActFunTrait<ActFunc>::function(mLMtx[mLMtx.size() - 2]);
-        arma::Mat<T> g = arma::strans(p) * d;
-        arma::Mat<T> w = mLearn * g + mMomentum * mDWMtx.back();
-        // if there is no hidden layer, we can update the weight matrix now
-        if (mWMtx.size() == 1){
-          mBMtx[0] += arma::sum(d);
-          mWMtx[0] += w;
-          mDWMtx[0] = w;
-          }
-    
-        // do the hidden layers in reverse iterative order,
-        // this loop only happens if there is >1 hidden layers
-        for (size_t i = mLMtx.size() - 2; mLMtx.size() > 2 && i > 0; --i){
-          mBMtx[i+1] += arma::sum(d);
-          d = ActFunTrait<ActFunc>::deriviative(mLMtx[i]) % (d * arma::strans(mWMtx[i+1]));
-          g = arma::strans(ActFunTrait<ActFunc>::function(mLMtx[i - 1])) * d;
-          mWMtx[i+1] += w;
-          mDWMtx[i+1] = w;
-          w = mLearn * g + mMomentum * mDWMtx[i];
-          }
-        // do the first layer if # hidden layer > 0
-        if (mLMtx.size() >= 2){
-          mBMtx[1] += arma::sum(d);
-          d = ActFunTrait<ActFunc>::deriviative(mLMtx[0]) % (d * arma::strans(mWMtx[1]));
-          g = arma::strans(in) * d;
-          mWMtx[1] += w;
-          mDWMtx[1] = w;
-          w = mLearn * g + mMomentum * mDWMtx[0];
-          mBMtx[0] += arma::sum(d);
-          mWMtx[0] += w;
-          mDWMtx[0] = w;
+          // feed forward step for back propagation
+          this->feedForward(in);
+  
+          arma::Mat<T> d = CostFunTrait<CostFunc,ActFunc,T>::deriviative(mLMtx.back(), out);
+          const arma::Mat<T>& p = mLMtx.size() == 1 ? in : ActFunTrait<ActFunc,T>::function(mLMtx[mLMtx.size() - 2]);
+          arma::Mat<T> g = arma::strans(p) * d;
+          arma::Mat<T> w = mLearn * g + mMomentum * mDWMtx.back();
+          // if there is no hidden layer, we can update the weight matrix now
+          if (mWMtx.size() == 1){
+            mBMtx[0] -= mLearn * arma::sum(d);
+            RegFunTrait<RegFunc>::regularize(mWMtx[0], w, regLearn, mRegu);
+            mDWMtx[0] = w;
+            }
+      
+          // do the hidden layers in reverse iterative order,
+          // this loop only happens if there is >1 hidden layers
+          for (size_t i = mLMtx.size() - 2; mLMtx.size() > 2 && i > 0; --i){
+            mBMtx[i+1] -= mLearn * arma::sum(d);
+            d = ActFunTrait<ActFunc,T>::deriviative(mLMtx[i]) % (d * arma::strans(mWMtx[i+1]));
+            g = arma::strans(ActFunTrait<ActFunc,T>::function(mLMtx[i - 1])) * d;
+            RegFunTrait<RegFunc>::regularize(mWMtx[i+1], w, regLearn, mRegu);
+            mDWMtx[i+1] = w;
+            w = mLearn * g + mMomentum * mDWMtx[i];
+            }
+          // do the first layer if # hidden layer > 0
+          if (mLMtx.size() >= 2){
+            mBMtx[1] -= mLearn * arma::sum(d);
+            d = ActFunTrait<ActFunc,T>::deriviative(mLMtx[0]) % (d * arma::strans(mWMtx[1]));
+            g = arma::strans(in) * d;
+            RegFunTrait<RegFunc>::regularize(mWMtx[1], w, regLearn, mRegu);
+            mDWMtx[1] = w;
+            w = mLearn * g + mMomentum * mDWMtx[0];
+            RegFunTrait<RegFunc>::regularize(mWMtx[0], w, regLearn, mRegu);
+            mBMtx[0] -= mLearn * arma::sum(d);
+            mWMtx[0] -= w;
+            mDWMtx[0] = w;
+            }
           }
         }
       }
-    };
-
-  template <template <typename> class ActFunc, template <typename> class InitFunc, typename T = double>
-  class RPROPFFNet : public FFNet<RPROPFFNet<ActFunc, InitFunc, T>, ActFunc, InitFunc, T> {
-    //TODO
     };
   }
