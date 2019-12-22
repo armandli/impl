5db47385c2a7dbc97717d03859f9dd259ae9d4b4
armandli <armand.li@hotmail.com>
2019-12-22 00:21:25 -0500

    add ceiling to a nearest power of 2 method

---
+++ bit_tricks/power.h
@@ -0,0 +1,11 @@
+// compute the nearest power of 2
+unsigned nearest_power_2(unsigned v){
+  v--;
+  v |= v >> 1U;
+  v |= v >> 2U;
+  v |= v >> 4U;
+  v |= v >> 8U;
+  v |= v >> 16U;
+  v++;
+  return v;
+}
---
+++ bit_tricks/test_power.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include <power.h>
+
+TEST(power, TestPower1){
+  unsigned h = nearest_power_2(10);
+  EXPECT_EQ(16, h);
+}
+
+TEST(power, TestPower2){
+  unsigned h = nearest_power_2(16);
+  EXPECT_EQ(16, h);
+}
+
+TEST(power, TestPower3){
+  unsigned h = nearest_power_2(1);
+  EXPECT_EQ(1, h);
+}
+
+TEST(power, TestPower4){
+  unsigned h = nearest_power_2(0);
+  EXPECT_EQ(0, h);
+}
+
+TEST(power, TestPower5){
+  unsigned h = nearest_power_2(255);
+  EXPECT_EQ(256, h);
+}
+
+TEST(power, TestPower6){
+  unsigned h = nearest_power_2(2);
+  EXPECT_EQ(2, h);
+}
+
